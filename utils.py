c5d20d16e62d7be13349e1612903e716f1acadc4
Intangere <intangere@space>
2021-06-20 02:51:09 +0000

    More type checking

--- utils.py
+++ utils.py
@@ -1,16 +1,16 @@
 from .config import MIN_PASSWORD_LEN, BRANCA_SECRET, BRANCA_TTL
 from .schemas import Payload
-
+from typing import Union
 from branca import Branca
 import bcrypt
 import msgpack
 
 branca = Branca(BRANCA_SECRET)
 
-def hash_password(password: bytes):
+def hash_password(password: bytes) -> bytes:
   return bcrypt.hashpw(password, bcrypt.gensalt())
 
-def check_password(password: bytes):
+def check_password(password: bytes) -> bool:
   if len(password) < MIN_PASSWORD_LEN:
     return False
 
@@ -22,10 +22,10 @@ def check_password(password: bytes):
 
   return True
 
-def verify_password(password: bytes, hashed: bytes):
+def verify_password(password: bytes, hashed: bytes) -> bool:
   return bcrypt.checkpw(password, hashed)
 
-def check_token(token: str, username: str):
+def check_token(token: str, username: str) -> Union[Payload, None]:
   try:
     payload = branca.decode(token, ttl=BRANCA_TTL)
     data = msgpack.unpackb(payload, raw=False)
@@ -37,7 +37,7 @@ def check_token(token: str, username: str):
   except RuntimeError as e:
     return
 
-def decode_token(token: str):
+def decode_token(token: str) -> Union[Payload, None]:
   try:
     payload = branca.decode(token, ttl=BRANCA_TTL)
     data = msgpack.unpackb(payload, raw=False)
@@ -46,7 +46,7 @@ def decode_token(token: str):
   except RuntimeError as e:
     return
 
-def create_token(username: str):
+def create_token(username: str) -> str:
   payload = msgpack.packb({'username': username})
   token = branca.encode(payload)
 
